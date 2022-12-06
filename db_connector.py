44a3a62e3db861d980f967c74d23dee53aeef6f6
WORKSTATION <SVS1170@gmail.com>
2022-12-06 16:07:35 +0300

    cycle_read & insert_data

--- db_connector.py
+++ db_connector.py
@@ -1,3 +1,5 @@
+import random
+import time
 import psycopg2
 import configparser
 
@@ -28,7 +30,7 @@ def create_table_data(name='data'):
   cur = con.cursor()
   cur.execute(f'''CREATE TABLE {name}  
        (Currentdatetime timestamp not null default CURRENT_TIMESTAMP PRIMARY KEY,
-       ADDRESS INT NOT NULL,
+       SERIAL INT NOT NULL,
        DESCRIPTION CHAR(50),
        VOLTAGE_A REAL,
        VOLTAGE_B REAL,
@@ -54,24 +56,33 @@ def create_table_data(name='data'):
        ACTIVE_ENERGY_T4 REAL,
        ACTIVE_ENERGY_A REAL,
        ACTIVE_ENERGY_B REAL,
-       ACTIVE_ENERGY_C REAL);''')
+       ACTIVE_ENERGY_C REAL,
+       CASETEMP REAL,
+       OUTDOORTEMP REAL);''')
   commit()
 
 
-def insert_data_counter(name="counter"):
+def insert_data_counter(name="counters"):
   cur = con.cursor()
   cur.execute(
-    f"INSERT INTO {name} (SERIAL,ADDRESS,DESCRIPTION) VALUES (41851891, 91, 'test')"
+    f"INSERT INTO {name} (SERIAL,ADDRESS,MODEL,DESCRIPTION) VALUES (13870485, 85,'MERCURY 230 ART-03M CLN', 'HOME');"
   )
   commit()
 
-def insert_data_data(name="data",):
+def insert_data_data(name="data",Ua = 0.00, Ub = 0.00, Uc = 0.00, Ia = 0.00, Ib = 0.00, Ic = 0.00, P = 0.00, Pa = 0.00, Pb = 0.00, Pc = 0.00, Qa = 0.00, Qb = 0.00, Qc = 0.00, Sa = 0.00, Sb = 0.00, Sc = 0.00, Tcase = 25.00, Tout = 20.00):
+  # con = psycopg2.connect(
+  #   database=config["database"]["db"],
+  #   user=config["database"]["user"],
+  #   password=config["database"]["passwd"],
+  #   host=config["database"]["host"],
+  #   port=int(config["database"]["port"])
+  # )
   cur = con.cursor()
   cur.execute(
-    f"INSERT INTO {name} (ADDRESS,DESCRIPTION,VOLTAGE_A,VOLTAGE_B,VOLTAGE_C,CURRENT_A,CURRENT_B,CURRENT_C,ACTIVE_POWER_FULL,"
+    f"INSERT INTO {name} (SERIAL,DESCRIPTION,VOLTAGE_A,VOLTAGE_B,VOLTAGE_C,CURRENT_A,CURRENT_B,CURRENT_C,ACTIVE_POWER_FULL,"
     f"ACTIVE_POWER_A,ACTIVE_POWER_B,ACTIVE_POWER_C,REACTIVE_POWER_A,REACTIVE_POWER_B,REACTIVE_POWER_C,FULL_POWER,FULL_POWER_A,"
     f"FULL_POWER_B,FULL_POWER_C,ACTIVE_ENERGY,ACTIVE_ENERGY_T1,ACTIVE_ENERGY_T2,ACTIVE_ENERGY_T3,ACTIVE_ENERGY_T4,ACTIVE_ENERGY_A,"
-    f"ACTIVE_ENERGY_B,ACTIVE_ENERGY_C) VALUES (91, 'test', 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00)"
+    f"ACTIVE_ENERGY_B,ACTIVE_ENERGY_C, CASETEMP, OUTDOORTEMP) VALUES (13870485, 'test', {Ua}, {Ub}, {Uc}, {Ia}, {Ib}, {Ic}, {P}, {Pa}, {Pb}, {Pc}, {Qa}, {Qb}, {Qc}, {Sa}, {Sb}, {Sc}, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, {Tcase}, {Tout})"
   )
   commit()
   print('ok')
@@ -97,10 +108,31 @@ def close():
 
 def commit():
   con.commit()
-  con.close()
+  # con.close()
 
-# create_table_counters('test')
-read_data('data')
+def test():
+  i = 0
+  while i <= 3599:
+    a = random.randrange(21000, 25000, 1)/100
+    b = random.randrange(21000, 25000, 1)/100
+    c = random.randrange(21000, 25000, 1)/100
+    ap = (50000+(i*10))/100
+    Pa = (50000 + (i * 10)) / 100
+    Pb = (51000 + (i * 10)) / 100
+    Pc = (52000 + (i * 10)) / 100
+    insert_data_data('data', a, b, c, ap, Pa, Pb, Pc)
+    i = i+1
+    time.sleep(10)
+    print(f"test data {i}")
+    if i==3600:
+      print("test data created")
+  close()
+  print("connection closed")
+
+# create_table_counters('counters')
+# read_data('data')
 # update_data('test')
 # create_table_data()
 # insert_data_data()
+# insert_data_counter('counters')
+test()
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import struct
 import time
 import configparser
 from mercury230 import Mercury230
+import db_connector as db
 from bitstring import BitArray
 
 config = configparser.ConfigParser()
@@ -20,14 +21,32 @@ mercury_234 = Mercury230(address, port)
 def cycle_read():
     while r == True:
         mercury_234.connect_user()
-        print("Ua : ", mercury_234.get_voltage_A(), ", Ub : ", mercury_234.get_voltage_B(), ", Uc : ", mercury_234.get_voltage_C())
-        print("Ia : ", mercury_234.get_current_A(), ", Ib : ", mercury_234.get_current_B(), ", Ic : ", mercury_234.get_current_C())
-        print("Pa : ", mercury_234.get_P_A(), ", Pb : ", mercury_234.get_P_B(), ", Pc : ", mercury_234.get_P_C())
-        print("Qa : ", mercury_234.get_Q_A(), ", Qb : ", mercury_234.get_Q_B(), ", Qc : ", mercury_234.get_Q_C())
-        print("Sa : ", mercury_234.get_S_A(), ", Sb : ", mercury_234.get_S_B(), ", Sc : ", mercury_234.get_S_C())
-        print("summPa,Pb,Pc :", mercury_234.get_active_energy_phases())
-        print(mercury_234.get_active_energy_current_day())
+        Ua = mercury_234.get_voltage_A()
+        Ub = mercury_234.get_voltage_B()
+        Uc = mercury_234.get_voltage_C()
+        Ia = mercury_234.get_current_A()
+        Ib = mercury_234.get_current_B()
+        Ic = mercury_234.get_current_C()
+        P = mercury_234.get_P()
+        Pa = mercury_234.get_P_A()
+        Pb = mercury_234.get_P_B()
+        Pc = mercury_234.get_P_C()
+        Qa = mercury_234.get_Q_A()
+        Qb = mercury_234.get_Q_B()
+        Qc = mercury_234.get_Q_C()
+        Sa = mercury_234.get_S_A()
+        Sb = mercury_234.get_S_B()
+        Sc = mercury_234.get_S_C()
+        Tcase = mercury_234.get_temp()
+        print("Ua : ", Ua, ", Ub : ", Ub, ", Uc : ", Uc)
+        print("Ia : ", Ia, ", Ib : ", Ib, ", Ic : ", Ic)
+        print("Pa : ", Pa, ", Pb : ", Pb, ", Pc : ", Pc)
+        print("Qa : ", Qa, ", Qb : ", Qb, ", Qc : ", Qc)
+        print("Sa : ", Sa, ", Sb : ", Sb, ", Sc : ", Sc)
+        # print("summPa,Pb,Pc :", mercury_234.get_active_energy_phases())
+        # print(mercury_234.get_active_energy_current_day())
+        db.insert_data_data('data', Ua, Ub, Uc, Ia, Ib, Ic, P, Pa, Pb, Pc, Qa, Qb, Qc, Sa, Sb, Sc, Tcase)
         mercury_234.disconnect()
-        time.sleep(1)
+        time.sleep(10)
 
 cycle_read()

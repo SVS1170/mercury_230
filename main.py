e8581e76b7f1935a687726265b5c6f2caae31361
Philosof-k <77165321+Philosof-k@users.noreply.github.com>
2023-03-02 17:23:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -101,7 +101,8 @@ def cycle_read():
         # print("summPa,Pb,Pc :", mercury_234.get_active_energy_phases())
         # print(mercury_234.get_active_energy_current_day())
 #        db.insert_data_data('data', Ua, Ub, Uc, Ia, Ib, Ic, P, Pa, Pb, Pc, Qa, Qb, Qc, Sa, Sb, Sc, Tcase)
-        json_string = '{"Ua": "' + str(Ua) + '", "Ub": "' + str(Ub) + '"}'
+        json_string = '{"Ua": "' + str(Ua) + '", "Ub": "' + str(Ub) +
+        '"}'
 #        json_object = json.loads(json_string)
         print(json_string)
         client.publish(mqtt_topic, json_string, 1)

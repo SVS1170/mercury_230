269624d1c59b45a1c299edca68d7cef20dc933d3
Philosof-k <77165321+Philosof-k@users.noreply.github.com>
2023-03-02 11:51:09 +0300

    Update mercury230.py

--- mercury230.py
+++ mercury230.py
@@ -13,8 +13,8 @@ class Mercury230:
         self.addr = struct.pack('B', address)
         self.ipaddress1 = ipaddress
         self.ipport1 = ipport
-        ser = self.open_port(self.ipaddress1, self.ipport1)
-        ser.timeout = 0.1
+#        ser = self.open_port(self.ipaddress1, self.ipport1)
+#        ser.timeout = 0.1
 
     def open_port(self, ipaddress1, ipport1):
 #        ser = serial.Serial(f"{port1}", 9600, serial.EIGHTBITS, serial.PARITY_NONE, serial.STOPBITS_ONE)
